a97f9ae469be1e43d86eca9410f499ca4e66a820
Patric Oare <poare@lqft.mit.edu>
2019-10-11 17:46:43 -0400

    slight modification to get_configs.py to make it more general

--- pion_mass/get_cfgs.py
+++ pion_mass/get_cfgs.py
@@ -3,7 +3,8 @@ from os.path import isfile, join
 
 #path = "/Users/poare/lqcd/pion_mass/"
 path = '/data/d10b/ensembles/isoClover/'
-cfgbase = "cl3_16_48_b6p1_m0p2450"
+#cfgbase = "cl3_16_48_b6p1_m0p2450"
+cfgbase = "cl3_32_48_b6p1_m0p2450"
 folder = path + cfgbase + "/cfgs/"
 
 print('Getting ids from directory: ' + folder)
@@ -12,6 +13,8 @@ filenames = [f for f in listdir(folder) if isfile(join(folder, f))]
 cfgIds = []
 for file in filenames:
     # assume filename format is '...cfg_idx.lime'
+    if file[-4:] != 'lime':
+        continue
     after_cfg = file.split('cfg_')[1]
     idx = after_cfg.split('.lime')[0]
     cfgIds.append(idx + "\n")
