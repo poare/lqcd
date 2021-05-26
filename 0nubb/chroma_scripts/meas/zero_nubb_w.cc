eb4830780e534c757d9b508646600ccd8dbfac80
Patrick Oare <poare@dhcp-10-31-103-145.dyn.MIT.EDU>
2021-05-26 16:24:43 -0400

    another bug fix

--- 0nubb/chroma_scripts/meas/zero_nubb_w.cc
+++ 0nubb/chroma_scripts/meas/zero_nubb_w.cc
@@ -124,7 +124,7 @@ void zero_nubb(const LatticePropagator& quark_prop_k1,
   for(int mu = 0; mu < Nd; mu++) {
     int gamIdx = (2 ** mu) - 1;
     GV[mu] = sumMulti(phase_mq * (antiprop_k2 * (Gamma(gamIdx) * quark_prop_k1)), dummyPhases.getSet())[0] / (double) vol;
-    GA[mu] = sumMulti(phase_mq * (antiprop_k2 * ((Gamma(gamIdx) * Gamma(G5)) * quark_prop_k1)), dummyPhases.getSet())[0] / (double) vol;
+    GA[mu] = sumMulti(phase_mq * (antiprop_k2 * (Gamma(gamIdx) * (Gamma(G5) * quark_prop_k1))), dummyPhases.getSet())[0] / (double) vol;
   }
 
   // Write current correlators
@@ -183,8 +183,8 @@ void zero_nubb(const LatticePropagator& quark_prop_k1,
 
 										Aab_comp = peekSpin(Aab, alpha, beta);
 										Acd_comp = peekSpin(Acd, rho, sigma);
-										Aad_comp = peekSpin(alpha, sigma);
-										Acb_comp = peekSpin(rho, beta);
+										Aad_comp = peekSpin(Aad, alpha, sigma);
+										Acb_comp = peekSpin(Acb, rho, beta);
 
 										Complex Gcomp = 2 * sumMulti(phase_m2q * (Aab_comp * Acd_comp - Aad_comp * Acb_comp), dummyPhases.getSet())[0] / (double) vol;
 
