a5a869b78d9d5b2185bdc8c4bebcaae277eae573
Patrick Oare <patrickoare@gmail.com>
2021-10-07 14:57:18 -0400

    restructured some code

--- spectral/python_scripts/nevanlinna.py
+++ spectral/python_scripts/nevanlinna.py
@@ -12,128 +12,39 @@
 # in a numpy array C.
 ################################################################################
 
-import numpy as np
-import gmpy2 as gmp
-import h5py
-import os
-import time
-import re
-import itertools
-import io
-import random
+from utils import *
 
-# Set precision for gmpy2 and initialize complex numbers
-prec = 128
-gmp.get_context().allow_complex = True
-gmp.get_context().precision = prec
+def main():
+    # Choose interpolant function theta_{M + 1}, and the grid to evaluate on. These parameters can all be varied.
+    Nreal = 6000
+    omega_bounds = [-10, 10]
+    eta = 1e-3
+    theta_mp1 = lambda z : 0            # change this based on priors
 
-# Mobius transform
-I = gmp.mpc(0, 1)
-h = lambda z : (z - I) / (z + I)
-hinv = lambda q : I * (gmp.mpc(1, 0) + q) / (gmp.mpc(1, 0) - q)
-# for some reason mpc segfaults when it uses the built in conjugate values, so just use this
-def conj(z):
-    return gmp.mpc(z.real, -z.imag)
+    # Data input with frequencies and values of G
+    data_path = '/Users/theoares/lqcd/spectral/hardy_optim_clean_submission/test/freqs_1.txt'
+    Y, C, lam, Npts = read_txt_input(data_path)
+    print('Read in ' + str(Npts) + ' Matsubara modes at frequencies ~ ' + str(["{0:1.8f}".format(x) for x in Y]) + '.')
+    for z in C:
+        assert z.imag > 0.0, 'Negative of input function is not Nevanlinna.'
+    Pick = construct_Pick(Y, lam)
+    print('Pick matrix: ')
+    print(Pick)
+    # TODO implement check for positive semidefinite-ness
 
-# Data input with frequencies and values of G
-data_path = '/Users/theoares/lqcd/spectral/hardy_optim_clean_submission/test/freqs_1.txt'
-Npts = 0
-Y = []    # Matsubara frequencies i\omega
-C = []    # Negative of G, NG
-with open(data_path) as f:
-    for line in f:
-        line = line.strip()
-        tokens = line.split()
-        # TODO depending on the input precision we may need to be more careful about the next line
-        freq, reG, imG = float(tokens[0]), float(tokens[1]), float(tokens[2])
-        Y.append(gmp.mpc(0, freq))
-        C.append(gmp.mpc(-reG, -imG))
-        Npts += 1
-    f.close()
-Y.reverse()         # code states that the algorithm is more robust with reverse order
-C.reverse()
-Y = np.array(Y)
-C = np.array(C)
-lam = [h(z) for z in C]
-print('Read in ' + str(Npts) + ' Matsubara modes at frequencies ~ ' + str(["{0:1.8f}".format(x) for x in Y]) + '.')
+    phi = construct_phis(Y, lam)
+    print('Phi[k] is: ' + str(phi))
+    zmesh = np.linspace(omega_bounds[0], omega_bounds[1], num = Nreal)
+    zspace = np.array([gmp.mpc(z, eta) for z in zmesh])
+    NGreal = analytic_continuation(Y, phi, zspace, theta_mp1)
 
-# Confirm that we're using a Nevanlinna function
-for z in C:
-    assert z.imag > 0.0, 'Negative of input function is not Nevanlinna.'
-
-# Construct Pick matrix to check that the Nevanlinna interpolant exists
-Pick = np.empty((Npts, Npts), dtype = object)
-for i, j in itertools.product(range(Npts), repeat = 2):
-    num = 1 - lam[i] * conj(lam[j])
-    denom = 1 - h(Y[i]) * conj(h(Y[j]))
-    Pick[i, j] = num / denom
-print('Pick matrix: ')
-print(Pick)
-
-# TODO implement check for positive semidefinite-ness
-
-# Construct phi[k] = theta_k(Y_k)
-abcd_bar_lst = []
-for k in range(Npts - 1):
-    id = np.array([
-        [gmp.mpc(1, 0), gmp.mpc(0, 0)],
-        [gmp.mpc(0, 0), gmp.mpc(1, 0)]
-    ])
-    abcd_bar_lst.append(id)
-phi = np.full((Npts), gmp.mpc(0, 0), dtype = object)
-phi[0] = lam[0]
-for k in range(Npts - 1):
-    for j in range(k, Npts - 1):
-        xik = (Y[j + 1] - Y[k]) / (Y[j + 1] - conj(Y[k]))
-        factor = np.array([
-            [xik, phi[k]],
-            [conj(phi[k]) * xik, gmp.mpc(1, 0)]
-        ])
-        abcd_bar_lst[j] = abcd_bar_lst[j] @ factor
-    num = lam[k + 1] * abcd_bar_lst[k][1, 1] - abcd_bar_lst[k][0, 1]
-    denom = abcd_bar_lst[k][0, 0] - lam[k + 1] * abcd_bar_lst[k][1, 0]
-    phi[k + 1] = num / denom
-print('Phi[k] is: ' + str(phi))
-
-# Choose interpolant function theta_{M + 1}, and the grid to evaluate on. These
-# parameters can all be varied.
-Nreal = 6000
-omega_min = -10
-omega_max = 10
-eta = 1e-3
-theta_mp1 = lambda z : 0            # change this based on priors
-
-# Perform interpolation by constructing a, b, c, d at each z-point.
-zmesh = np.linspace(omega_min, omega_max, num = Nreal)
-zspace = np.array([gmp.mpc(z, eta) for z in zmesh])
-
-# confirm that NGreal extends NG-- the last Npts values of NGreal should be C
-# zspace = np.append(zspace, Y)
-# Nreal += len(Y)
-
-NGreal = np.empty((Nreal), dtype = object)
-for idx, z in enumerate(zspace):
-    abcd = np.array([
-        [gmp.mpc(1, 0), gmp.mpc(0, 0)],
-        [gmp.mpc(0, 0), gmp.mpc(1, 0)]
-    ])
+    # confirm this is a valid analytic continuation-- it extends the input points
+    test_cont = analytic_continuation(Y, phi, Y, theta_mp1)
     for k in range(Npts):
-        xikz = (z - Y[k]) / (z - conj(Y[k]))
-        factor = np.array([
-            [xikz, phi[k]],
-            [conj(phi[k]) * xikz, gmp.mpc(1, 0)]
-        ])
-        abcd = abcd @ factor
-    num = abcd[0, 0] * theta_mp1(z) + abcd[0, 1]
-    denom = abcd[1, 0] * theta_mp1(z) + abcd[1, 1]
-    theta = num / denom         # contractive function theta(z)
-    NGreal[idx] = hinv(theta)
+        assert abs(test_cont[k] - C[k]) < gmp.mpfr(1e-15)
 
-out_path = '/Users/theoares/Dropbox (MIT)/research/spectral/testing/output_1.txt'
-f = open(out_path, 'w')
-header = f'{Nreal:.0f} {omega_min:.0f} {omega_max:.0f} {eta:.5f}\n'
-f.write(header)
-for i, NGval in enumerate(NGreal):
-    s =  NGval.__str__() + '\n'
-    f.write(s)
-f.close()
+    out_path = '/Users/theoares/Dropbox (MIT)/research/spectral/testing/output_1.txt'
+    write_txt_output(out_path, NGreal, Nreal, omega_bounds, eta)
+
+if __name__ == '__main__':
+    main()
---
+++ spectral/python_scripts/utils.py
@@ -0,0 +1,201 @@
+import numpy as np
+import gmpy2 as gmp
+import h5py
+import os
+import time
+import re
+import itertools
+import io
+import random
+
+# Set precision for gmpy2 and initialize complex numbers
+prec = 128
+gmp.get_context().allow_complex = True
+gmp.get_context().precision = prec
+
+# Mobius transform
+I = gmp.mpc(0, 1)
+h = lambda z : (z - I) / (z + I)
+hinv = lambda q : I * (gmp.mpc(1, 0) + q) / (gmp.mpc(1, 0) - q)
+# for some reason mpc segfaults when it uses the built in conjugate values, so just use this
+def conj(z):
+    """Returns the conjugate of a gmp.mpc value z. For some reason, the built in mpc function .conj
+    breaks when it is called."""
+    return gmp.mpc(z.real, -z.imag)
+
+def read_txt_input(data_path):
+    """
+    Reads correlator input from a text file and outputs the frequencies the correlator is
+    evaluated at, the correlator values at these frequencies, and the Mobius transform of the
+    correlator values.
+
+    Parameters
+    ----------
+    datapath : string
+        Path to input. Should be a text file.
+
+    Returns
+    -------
+    np.array[gmp.mpc]
+        Matsubara frequencies Y.
+    np.array[gmp.mpc]
+        Correlator values C.
+    np.array[gmp.mpc]
+        Mobius transformed correlator values lambda.
+    int
+        Number of points the correlator is evaluated at, i.e. card(C)
+    """
+    Npts = 0
+    Y = []    # Matsubara frequencies i\omega
+    C = []    # Negative of G, NG
+    with open(data_path) as f:
+        for line in f:
+            line = line.strip()
+            tokens = line.split()
+            # TODO depending on the input precision we may need to be more careful about the next line
+            freq, reG, imG = float(tokens[0]), float(tokens[1]), float(tokens[2])
+            Y.append(gmp.mpc(0, freq))
+            C.append(gmp.mpc(-reG, -imG))
+            Npts += 1
+        f.close()
+    Y.reverse()         # code states that the algorithm is more robust with reverse order
+    C.reverse()
+    Y = np.array(Y)
+    C = np.array(C)
+    lam = [h(z) for z in C]
+    return Y, C, lam, Npts
+
+def construct_Pick(Y, lam):
+    """
+    Constructs the Pick matrix (1 - lambda_i lambda_j^*) / (1 - h(Y_i) h(Y_j)^*) from correlator input
+    to check that the Nevanlinna interpolant exists.
+
+    Parameters
+    ----------
+    Y : np.array[gmp.mpc]
+        Matsubara frequencies the correlator is measured at.
+    lam : np.array[gmp.mpc]
+        Mobius transform of the input correlator.
+
+    Returns
+    -------
+    np.array[gmp.mpc]
+        len(lam) by len(lam) array for the Pick matrix.
+    """
+    Npts = len(Y)
+    Pick = np.empty((Npts, Npts), dtype = object)
+    for i, j in itertools.product(range(Npts), repeat = 2):
+        num = 1 - lam[i] * conj(lam[j])
+        denom = 1 - h(Y[i]) * conj(h(Y[j]))
+        Pick[i, j] = num / denom
+    return Pick
+
+def construct_phis(Y, lam):
+    """
+    Constructs the phi[k] values needed to perform the Nevanlinna analytic continuation.
+    At each iterative step k, phi[k] is defined as theta_k(Y_k), where theta_k is the kth
+    iterative approximation to the continuation.
+
+    Parameters
+    ----------
+    Y : np.array[gmp.mpc]
+        Matsubara frequencies the correlator is measured at.
+    lam : np.array[gmp.mpc]
+        Mobius transform of the input correlator.
+
+    Returns
+    -------
+    np.array[gmp.mpc]
+        Values of phi[k] = theta_k(Y_k).
+    """
+    Npts = len(Y)
+    abcd_bar_lst = []
+    for k in range(Npts - 1):
+        id = np.array([
+            [gmp.mpc(1, 0), gmp.mpc(0, 0)],
+            [gmp.mpc(0, 0), gmp.mpc(1, 0)]
+        ])
+        abcd_bar_lst.append(id)
+    phi = np.full((Npts), gmp.mpc(0, 0), dtype = object)
+    phi[0] = lam[0]
+    for k in range(Npts - 1):
+        for j in range(k, Npts - 1):
+            xik = (Y[j + 1] - Y[k]) / (Y[j + 1] - conj(Y[k]))
+            factor = np.array([
+                [xik, phi[k]],
+                [conj(phi[k]) * xik, gmp.mpc(1, 0)]
+            ])
+            abcd_bar_lst[j] = abcd_bar_lst[j] @ factor
+        num = lam[k + 1] * abcd_bar_lst[k][1, 1] - abcd_bar_lst[k][0, 1]
+        denom = abcd_bar_lst[k][0, 0] - lam[k + 1] * abcd_bar_lst[k][1, 0]
+        phi[k + 1] = num / denom
+    return phi
+
+def analytic_continuation(Y, phi, zspace, theta_mp1 = lambda z : 0):
+    """
+    Performs an analytic continuation assuming the function is Nevanlinna according to the
+    algorithm prescribed in the paper.
+
+    Parameters
+    ----------
+    Y : np.array[gmp.mpc]
+        Matsubara frequencies the correlator is measured at.
+    phi : np.array[gmp.mpc]
+        Vector of phi[k] = theta_k(Y_k) values to input.
+    zspace : np.array[np.float64]
+        Linspace to evaluate the analytic continuation at.
+    theta_mp1 : Function
+        Free function theta_{M + 1}(z) to fix in the algorithm.
+
+    Returns
+    -------
+    np.array[gmp.mpc]
+        len(zspace) array of values for the analytic continuation performed on zspace.
+    """
+    Nreal, Npts = len(zspace), len(Y)
+    cont = np.empty((Nreal), dtype = object)
+    for idx, z in enumerate(zspace):
+        abcd = np.array([
+            [gmp.mpc(1, 0), gmp.mpc(0, 0)],
+            [gmp.mpc(0, 0), gmp.mpc(1, 0)]
+        ])
+        for k in range(Npts):
+            xikz = (z - Y[k]) / (z - conj(Y[k]))
+            factor = np.array([
+                [xikz, phi[k]],
+                [conj(phi[k]) * xikz, gmp.mpc(1, 0)]
+            ])
+            abcd = abcd @ factor
+        num = abcd[0, 0] * theta_mp1(z) + abcd[0, 1]
+        denom = abcd[1, 0] * theta_mp1(z) + abcd[1, 1]
+        theta = num / denom         # contractive function theta(z)
+        cont[idx] = hinv(theta)
+    return cont
+
+def write_txt_output(out_path, data, Nreal, omega, eta):
+    """
+    Writes output from analytic continuation to a text file.
+
+    Parameters
+    ----------
+    out_path : string
+        Output file path to write to. Should be a .txt file.
+    data : np.array[gmp.mpc]
+        Datapoints for function to write to file.
+    Nreal : int
+        Size of linspace that the function is evaluated on.
+    omega : list[int]
+        Pair of values [omega_min, omega_max] with the minimum and maximum values of omega.
+    eta : float
+        Imaginary part of evaluation line, i.e. the function is evaluated at f(x + i\eta) with x real.
+    Returns
+    -------
+    """
+    f = open(out_path, 'w')
+    header = f'{Nreal:.0f} {omega[0]:.0f} {omega[1]:.0f} {eta:.5f}\n'
+    f.write(header)
+    for i, val in enumerate(data):
+        s =  val.__str__() + '\n'
+        f.write(s)
+    f.close()
+    return True
