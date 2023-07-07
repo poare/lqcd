21584ef227824603f73f6b703023214bff9c50d1
Patrick Oare <patrickoare@gmail.com>
2023-07-07 15:57:35 -0400

    tested some SU(N) generation functions

--- utilities/suNtools.py
+++ utilities/suNtools.py
@@ -12,7 +12,8 @@
 ################################################################################
 
 from __main__ import *
-n_boot = n_boot
+# n_boot = n_boot
+n_boot = 200
 
 import numpy as np
 import h5py
@@ -29,26 +30,84 @@ def vec_dot(u, v):
 def vec_norm(u):
     return np.sqrt(np.abs(vec_dot(u, u)))
 
+def dagger(M):
+    """Takes the Hermitian conjugate of a complex matrix M."""
+    return M.transpose().conj()
+
 def proj_vec(u, v):
     """Projects v onto linear subspace spanned by u."""
     return vec_dot(v, u) / vec_dot(u, u) * u
 
-def proj_SU3(M):
-    """
-    Projects a matrix M to the group SU(3) by orthonormalizing the first two columns, then 
-    taking a cross product.
-    """
-    N = M.copy()
-    [v1, v2, v3] = M.T
-    u1 = v1 / vec_norm(v1)              # normalize
-    u2 = v2 - proj_vec(u1, v2)
-    u2 = u2 / vec_norm(u2)
-    u3 = np.cross(u1.conj(), u2.conj())
-    return np.array([u1, u2, u3], dtype = np.complex64).T
+# def proj_SU3(M):
+#     """
+#     Projects a matrix M to the group SU(3) by orthonormalizing the first two columns, then 
+#     taking a cross product.
+#     """
+#     [v1, v2, v3] = M.T
+#     u1 = v1 / vec_norm(v1)              # normalize
+#     u2 = v2 - proj_vec(u1, v2)
+#     u2 = u2 / vec_norm(u2)
+#     u3 = np.cross(u1.conj(), u2.conj())
+#     return np.array([u1, u2, u3], dtype = np.complex128).T
 
-def rand_su3_matrix(eps):
+# def rand_su3_matrix(eps):
+#     """
+#     Generates a random SU(3) matrix for the metropolis update with parameter eps. 
+#     Follows Peter Lepage's notes.
+
+#     Parameters
+#     ----------
+#     eps : float
+#         Metropolis parameter for update candidate.
+    
+#     Returns
+#     -------
+#     np.array [Nc, Nc]
+#         Metropolis update candidate.
+#     """
+#     mat_elems = np.random.uniform(low = -1, high = 1, size = 6)
+#     H = np.array([
+#         [mat_elems[0], mat_elems[1], mat_elems[2]], 
+#         [mat_elems[1], mat_elems[3], mat_elems[4]], 
+#         [mat_elems[2], mat_elems[4], mat_elems[5]]
+#     ], dtype = np.complex64)
+#     return proj_SU3(np.eye(3) + 1j*eps*H)
+
+def proj_fund_suN(M, prec = 1e-8):
     """
-    Generates a random SU(3) matrix for the metropolis update with parameter eps. 
+    Projects a N x N matrix onto the fundamental representation of SU(N) by using the Gram-Schmidt procedure. 
+
+    Parameters
+    ----------
+    M : np.array [N, N], dtype = np.complex128
+        Matrix to project to SU(N).
+    
+    Returns
+    -------
+    proj(M) : np.array [N, N], dtype = np.complex128
+        Projection of mat. 
+    """
+    N = M.shape[0]
+    assert M.shape == (N, N), 'Matrix to project must be square.'
+    cols = [M[:, i] for i in range(N)]
+    for i in range(N):
+        vi = cols[i]
+        ui = vi.copy()
+        for k in range(i):
+            # cols[i] = cols[i] - proj_vec(u, v)
+            ui -= proj_vec(cols[k], vi)
+        cols[i] = ui / vec_norm(ui)
+    U = np.array(cols, dtype = np.complex128).T
+    detU = np.linalg.det(U)
+    if np.abs(detU - 1) < prec:
+        return U
+    assert np.abs(np.abs(detU) - 1) < prec, '|det U| is not 1.'
+    V = detU**(-1/N) * U
+    return V
+
+def rand_suN_matrix(N, eps):
+    """
+    Generates a random SU(N) matrix for the metropolis update with parameter eps. 
     Follows Peter Lepage's notes.
 
     Parameters
@@ -58,13 +117,19 @@ def rand_su3_matrix(eps):
     
     Returns
     -------
-    np.array [Nc, Nc]
+    np.array [N, N]
         Metropolis update candidate.
     """
-    mat_elems = np.random.uniform(low = -1, high = 1, size = 6)
-    H = np.array([
-        [mat_elems[0], mat_elems[1], mat_elems[2]], 
-        [mat_elems[1], mat_elems[3], mat_elems[4]], 
-        [mat_elems[2], mat_elems[4], mat_elems[5]]
-    ], dtype = np.complex64)
-    return proj_SU3(np.eye(3) + 1j*eps*H)
+    num = N * (N + 1) // 2               # number of real parameters for su(N)
+    mat_elems = list(np.random.uniform(low = -1, high = 1, size = num))
+    H = np.zeros((N, N), dtype = np.complex128)
+    for i in range(N):
+        elem = mat_elems.pop()
+        H[i, i] = elem
+    for i in range(N):
+        for j in range(i):
+            elem = mat_elems.pop()
+            H[i, j] = elem
+            H[j, i] = elem
+    assert len(mat_elems) == 0
+    return proj_fund_suN(np.eye(N) + 1j*eps*H)
---
+++ utilities/testing/test_suNtools.py
@@ -0,0 +1,68 @@
+################################################################################
+# Tests an implementation of RHMC.                                             #
+# Tests to perform:                                                            #
+#   0. Test accept-reject of each possibility.                                 #
+#   1. Gauge coupling g --> 0 limit (free theory)                              #
+#   2. g --> and m --> infinity (pure gauge theory)                            #
+################################################################################
+# Author: Patrick Oare                                                         #
+################################################################################
+
+n_boot = 200
+import numpy as np
+import itertools
+import scipy
+from scipy.sparse import bsr_matrix
+
+np.random.seed(20)
+
+import sys
+sys.path.append('/Users/theoares/lqcd/utilities')
+import suNtools
+
+EPS = 1e-8
+
+def print_line():
+    print('-'*50)
+
+def is_equal(a, b, eps = EPS):
+    """Returns whether a and b are equal up to precision eps."""
+    return np.abs(a - b) < eps
+
+def test_proj_suN(n_samps = 500):
+    """Tests that matrices projected with suNtools.proj_fund_suN are in fact, in SU(N)."""
+    for N in range(2, 10):
+        for i in range(n_samps):
+            M = np.random.rand(N, N) + 1j * np.random.rand(N, N)
+            U = suNtools.proj_fund_suN(M, prec = EPS)
+            assert np.allclose(U @ suNtools.dagger(U), np.eye(N), atol = EPS), 'U is not unitary.'
+            assert np.allclose(suNtools.dagger(U) @ U, np.eye(N), atol = EPS), 'U is not unitary.'
+            assert is_equal(np.linalg.det(U), 1), 'U does not have unit determinant.'
+    print('test_proj_suN : Pass')
+
+def test_gen_suN(n_samps = 100):
+    """Tests that matrices generated with suNtools.rand_suN_matrix are in fact, in SU(N)."""
+    for N in range(2, 10):
+        for eps in np.arange(0.1, 0.9, 0.1):
+            for i in range(n_samps):
+                U = suNtools.rand_suN_matrix(N, eps)
+                assert np.allclose(U @ suNtools.dagger(U), np.eye(N), atol = EPS), 'U is not unitary.'
+                assert np.allclose(suNtools.dagger(U) @ U, np.eye(N), atol = EPS), 'U is not unitary.'
+                assert is_equal(np.linalg.det(U), 1), 'U does not have unit determinant.'
+    print('test_gen_suN : Pass')
+
+################################################################################
+################################## RUN TESTS ###################################
+################################################################################
+
+print_line()
+print('RUNNING TESTS')
+print_line()
+
+test_proj_suN()
+test_gen_suN()
+
+print_line()
+print('ALL TESTS PASSED')
+print_line()
+
