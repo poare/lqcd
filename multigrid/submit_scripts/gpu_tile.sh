64c88a076615cfdaf5ee33b083716d0ca0f075cb
Patrick Oare <patrickoare@gmail.com>
2026-02-23 11:56:41 -0500

    saving gpu tile

---
+++ multigrid/submit_scripts/gpu_tile.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#export NUMA_MAP=(2 2 2 3 3 3 2 2 2 3 3 3 )
+#export NUMA_MAP=(0 0 1 1 0 0 1 1 0 0 1 1);
+#export  GPU_MAP=(0.0 0.1 3.0 3.1 1.0 1.1 4.0 4.1 2.0 2.1 5.0 5.1)
+
+export NUMA_PMAP=(0 0 1 1 0 0 1 1 );
+export  GPU_MAP=(0.0 1.0 3.0 4.0 0.1 1.1 3.1 4.1 )
+
+export NUMAP=${NUMA_PMAP[$PALS_LOCAL_RANKID]}
+export gpu_id=${GPU_MAP[$PALS_LOCAL_RANKID]}
+  
+unset EnableWalkerPartition
+export EnableImplicitScaling=0
+export ZE_AFFINITY_MASK=$gpu_id
+export ONEAPI_DEVICE_FILTER=gpu,level_zero
+
+export SYCL_PI_LEVEL_ZERO_DEVICE_SCOPE_EVENTS=0
+export SYCL_PI_LEVEL_ZERO_USE_IMMEDIATE_COMMANDLISTS=1
+export SYCL_PI_LEVEL_ZERO_USE_COPY_ENGINE=0:4
+export SYCL_PI_LEVEL_ZERO_USE_COPY_ENGINE_FOR_D2D_COPY=1
+
+echo "rank $PALS_RANKID ; local rank $PALS_LOCAL_RANKID ; ZE_AFFINITY_MASK=$ZE_AFFINITY_MASK ; NUMA $NUMA "
+
+numactl -p $NUMAP -N $NUMAP  "$@"
