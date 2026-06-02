080143eacf3a17737efb4263a8059b20b1519926
Patrick Oare <patrickoare@gmail.com>
2026-06-02 10:10:36 -0400

    updating repo

--- multigrid/scripts/run_kryschur_spec.sh
+++ multigrid/scripts/run_kryschur_spec.sh
@@ -1,5 +1,7 @@
 #!bin/bash
 
+TIME=$(date +%F_%T)
+
 GRID=/Users/patrickoare/libraries/Grid
 GRIDINSTALL=/Users/patrickoare/libraries/GridInstall
 LQCD=/Users/patrickoare/lqcd
@@ -9,22 +11,29 @@ make -C examples && make install -C examples
 
 cd ${GRIDINSTALL}/bin
 
-Nm=20
-Nk=12
-maxIter=10000
-Nstop=10
+Nm=100
+Nk=50
+maxIter=4
+Nstop=8
 
 root="ckpoint_EODWF_lat.125"
-dirname="${root}_Nm${Nm}_Nk${Nk}"
+# runname="Nm${Nm}_Nk${Nk}_${TIME}"
+runname="unprec_Nm${Nm}_Nk${Nk}_${TIME}"
 
-rf="EvalReSmall"
+homedir=/Users/patrickoare
+inFile="${homedir}/libraries/PETSc-Grid/${root}"
 
-inFile="/Users/patrickoare/libraries/PETSc-Grid/${root}"
-logs="/Users/patrickoare/lqcd/multigrid/logs/${dirname}.out"
-outDir="/Users/patrickoare/lqcd/multigrid/spectra/${dirname}"
+logDir="${homedir}/lqcd/multigrid/logs/${root}"
+outDir="${homedir}/lqcd/multigrid/spectra/${root}/${runname}"
 
+mkdir -p ${logDir}
 mkdir -p ${outDir}
 
-./Example_spec_kryschur ${Nm} ${Nk} ${maxIter} ${Nstop} ${inFile} ${outDir} ${rf} > ${logs}
+logs="${logDir}/${runname}.log"
+rf="EvalReSmall"
+# rf="EvalReLarge"
+
+# ./Example_spec_kryschur ${Nm} ${Nk} ${maxIter} ${Nstop} ${inFile} ${outDir} ${rf} > ${logs}
+./Example_spec_kryschur ${Nm} ${Nk} ${maxIter} ${Nstop} ${inFile} ${outDir} ${rf}
 
 cd ${LQCD}/multigrid/scripts
--- spectral/python_scripts/nevanlinna.py
+++ spectral/python_scripts/nevanlinna.py
@@ -15,6 +15,9 @@
 from utils import *
 
 def main():
+
+    root = r'/Users/patrickoare'
+
     # Choose interpolant function theta_{M + 1}, and the grid to evaluate on. These parameters can all be varied.
     Nreal = 6000
     omega_bounds = [-10, 10]
@@ -22,9 +25,10 @@ def main():
     theta_mp1 = lambda z : 0            # change this based on priors
 
     # Data input with frequencies and values of G
-    data_path = '/Users/theoares/lqcd/spectral/hardy_optim_clean_submission/test/freqs_1.txt'
+    data_path = f'{root}/lqcd/spectral/hardy_optim_clean_submission/test/freqs_1.txt'
     Y, C, lam, Npts = read_txt_input(data_path)
-    print('Read in ' + str(Npts) + ' Matsubara modes at frequencies ~ ' + str(["{0:1.8f}".format(x) for x in Y]) + '.')
+    # print('Read in ' + str(Npts) + ' Matsubara modes at frequencies ~ ' + str(["{0:1.8f}".format(x) for x in Y]) + '.')
+    print('Read in ' + str(Npts) + ' Matsubara modes at frequencies ~ ' + str([mp.nstr(x, 8) for x in Y]) + '.')
     for z in C:
         assert z.imag > 0.0, 'Negative of input function is not Nevanlinna.'
     Pick = construct_Pick(Y, lam)
@@ -35,15 +39,17 @@ def main():
     phi = construct_phis(Y, lam)
     print('Phi[k] is: ' + str(phi))
     zmesh = np.linspace(omega_bounds[0], omega_bounds[1], num = Nreal)
-    zspace = np.array([gmp.mpc(z, eta) for z in zmesh])
+    zspace = np.array([mp.mpc(z, eta) for z in zmesh])
     NGreal = analytic_continuation(Y, phi, zspace, theta_mp1)
 
     # confirm this is a valid analytic continuation-- it extends the input points
     test_cont = analytic_continuation(Y, phi, Y, theta_mp1)
     for k in range(Npts):
-        assert abs(test_cont[k] - C[k]) < gmp.mpfr(1e-15)
+        # assert abs(test_cont[k] - C[k]) < gmp.mpfr(1e-15)
+        err = abs(test_cont[k] - C[k])
+        assert err < mp.mpf(1e-14), f'error is: {err}'
 
-    out_path = '/Users/theoares/Dropbox (MIT)/research/spectral/testing/output_1.txt'
+    out_path = f'{root}/Dropbox (MIT)/research/spectral/testing/output_1.txt'
     write_txt_output(out_path, NGreal, Nreal, omega_bounds, eta)
 
 if __name__ == '__main__':
